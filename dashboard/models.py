7910751671ce5126b8d6c25afde77d1ed5b85533
Akshay Thakur <akshay.181004@gmail.com>
2021-10-28 16:47:40 +0530

    Update models.py

--- dashboard/models.py
+++ dashboard/models.py
@@ -10,13 +10,3 @@ class datamodel(models.Model):
         DaysOverdue= models.IntegerField()
         AmountOutstanding= models.IntegerField()
         Recovery= models.IntegerField()
-'''
-class viewmodel(models.Model):
-
-        Date= models.CharField(max_length=10)
-        ChartType= models.CharField(max_length=30)
-        CustomerId= models.IntegerField()
-        CustomerName= models.CharField(max_length=30)
-        DaysOverdue= models.IntegerField()
-        AmountOutstanding= models.IntegerField()
-        Recovery= models.IntegerField()'''
